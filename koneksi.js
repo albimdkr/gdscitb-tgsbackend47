5b87dcbaee9011666b2becebd1766a16b50900c7
albi mudakar <albimudakar644@gmail.com>
2022-01-24 18:30:51 +0700

    menambahkan file koneksi

---
+++ koneksi.js
@@ -0,0 +1,19 @@
+var mysql = require ('mysql');
+
+//membuat koneksi ke database
+
+const conn = mysql.createConnection ({
+    host:'localhost',
+    user: 'root',
+    password : 'albi-mdkr-2112',
+    database: 'dbfilm'
+});
+
+//error kondisi
+
+conn.connect ((err)=>{
+    if (err) throw err;
+    console.log('My sql success connect');
+});
+
+module.exports = conn;
