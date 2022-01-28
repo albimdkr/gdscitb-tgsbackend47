3c431870f15e435ac1f16fab5edb91f6cc176bea
albi mudakar <albimudakar644@gmail.com>
2022-01-28 21:12:07 +0700

    menambahkan methode POST

--- controller.js
+++ controller.js
@@ -8,7 +8,7 @@ exports.index = function (req,res){
 };
 
 
-//menampilkan semua data user
+//menampilkan semua data user GET 
 exports.tampilsemuauser = function (req,res){
     connection.query('SELECT * FROM user', function(error, rows, fields){
         if (error){
@@ -20,7 +20,7 @@ exports.tampilsemuauser = function (req,res){
     });
 };
 
-//menampilka semua data user berdasarkan id
+//menampilkan semua data user berdasarkan id GET ID
 exports.tampilberdasarkanid = function (req,res){
     let id = req.params.id;
     connection.query('SELECT * FROM user WHERE id = ?', [id], 
@@ -31,4 +31,21 @@ exports.tampilberdasarkanid = function (req,res){
             response.ok(rows, res); 
         }
     });
+};
+
+//menambahkan data user POST
+exports.tambahUser = function (req, res){
+    var uname = req.body.uname;
+    var pwd = req.body.pwd;
+
+    connection.query('INSERT INTO user (uname,pwd) VALUES (?,?)',
+    [uname, pwd],
+    function (error, rows, fields){
+        if (error){
+            console.log(error);
+        } else {
+            response.ok("Berhasil menambahkan data user", res)
+        }
+    });
+
 };
--- routes.js
+++ routes.js
@@ -6,10 +6,12 @@ module.exports = function (app){
     app.route('/')
         .get(jsonku.index);
 
-
     app.route('/tampil')
         .get(jsonku.tampilsemuauser); 
 
     app.route('/tampil/:id')
         .get(jsonku.tampilberdasarkanid);
+
+    app.route('/tambah')
+        .post(jsonku.tambahUser);
 };
