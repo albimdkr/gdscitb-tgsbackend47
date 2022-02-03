78aa1d6637c776f28661f39fbd7c60213f3b204a
albi mudakar <albimudakar644@gmail.com>
2022-02-03 20:40:30 +0700

    add register auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+var connection = require ('../koneksi');
+var mysql = require ('mysql');
+var md5 = require ('MDS');
+var response = require ('../res');
+var jwt = require ('jsonwebtoken');
+var config = require ('../config/secret');
+var ip = require ('ip');
+
+// controller  untuk register 
+exports.registrasi = function (req, res){
+    var post = {
+        uname : req.body.uname,
+        pwd  : md5(req.body.pwd),
+    }
+
+    var query = "SELECT uname FROM ?? WHERE ??";
+    var table = ["user", "uname", post.uname];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function(error, rows) {
+        if (error){
+            console.log(error);
+        }else {
+            if(rows.length == 0){
+                var query = "INSERT INTO ?? SET";
+                var table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows){
+                     if (error){
+                         console.log(error);
+                     } else {
+                          response.ok("Berhasil menambahkan data user baru", res);
+                     }
+                });  
+            }else {
+                response.ok ("username sudah terdaftar");
+            }
+        }
+    })
+} 
